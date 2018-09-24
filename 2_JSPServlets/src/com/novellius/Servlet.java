c87cc51c2502ad596db7bd70e03b024d6a6bd9ab
jhingx1 <jhingx1@gmail.com>
2018-09-24 18:06:49 -0500

    Parametros iniciales.

--- 2_JSPServlets/src/com/novellius/Servlet.java
+++ 2_JSPServlets/src/com/novellius/Servlet.java
@@ -3,6 +3,7 @@ package com.novellius;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +12,10 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 public class Servlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+	
+	private static final long serialVersionUID = 1L;	
+	
+	private String rutaJsp;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -20,6 +24,17 @@ public class Servlet extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
+    
+    //Implementar el metodo init, para inicializar parametros iniciales
+    @Override
+	public void init(ServletConfig config) throws ServletException {	
+		super.init(config);
+		//Parametro inicial
+		//System.out.println(config.getInitParameter("rutaJsp"));//esta mapeado en el web.xml
+		rutaJsp = config.getInitParameter("rutaJsp");
+		System.out.println(rutaJsp);
+	}
+    
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -29,9 +44,9 @@ public class Servlet extends HttpServlet {
 		if(accion!=null){
 			
 			if(accion.equals("login")){
-				getServletContext().getRequestDispatcher("/jsp/login.jsp").forward(request, response);
+				getServletContext().getRequestDispatcher(rutaJsp + "login.jsp").forward(request, response);
 			}else if(accion.equals("inicio")){
-				getServletContext().getRequestDispatcher("/jsp/index.jsp").forward(request, response);
+				getServletContext().getRequestDispatcher(rutaJsp + "index.jsp").forward(request, response);
 			}
 			/*
 			else if(accion.equals("iniciarSesion")){
@@ -41,9 +56,11 @@ public class Servlet extends HttpServlet {
 			
 		}else{
 			//Contexto del servlet
-			getServletContext().getRequestDispatcher("/jsp/index.jsp").forward(request, response);
+			getServletContext().getRequestDispatcher(rutaJsp + "index.jsp").forward(request, response);
 		}
 	}
+
+	
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
@@ -76,12 +93,12 @@ public class Servlet extends HttpServlet {
 				contexto.setAttribute("usuario", usuario);
 				contexto.setAttribute("contrasena", contrasena);
 				
-				getServletContext().getRequestDispatcher("/jsp/postLogin.jsp").forward(request, response);
+				getServletContext().getRequestDispatcher(rutaJsp + "postLogin.jsp").forward(request, response);
 			}
 		}	
 		else{
 			//Contexto del servlet
-			getServletContext().getRequestDispatcher("/jsp/index.jsp").forward(request, response);
+			getServletContext().getRequestDispatcher(rutaJsp + "index.jsp").forward(request, response);
 		}
 	}
 
