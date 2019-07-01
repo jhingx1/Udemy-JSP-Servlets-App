c914df3033cb5c5d02cc00b60bbaf011a4bf5523
jhingx1 <jhingx1@gmail.com>
2019-07-01 09:00:03 -0500

    AppWeb Adminlogn
    AppWeb Adminlogn

---
+++ cliente2-negocio-Ujs-adminlogin/src/com/negocio/Servlet.java
@@ -0,0 +1,42 @@
+package com.negocio;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @see Servlet Recibe peticiones de internet.
+ * @see Ciclo de vida del servlet : leer notas-cicloServlet
+ * @author Mark1
+ *
+ */
+
+public class Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet() {
+        super();        
+    }
+
+	/**
+	 * @see 
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see 
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
